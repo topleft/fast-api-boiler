edc96ed0b66dc8445fc8bad4b22c0370d4c2525a
Pete Jeffryes <pjeffryes@tendrilinc.com>
2020-11-03 10:55:59 -0600

    lots of refactoring

--- db/migrations/scripts/downgrade_db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export DB_URI=localhost
-export DB_PORT=5433
-export DB_NAME=boiler
-export DB_USER=boiler_user
-export DB_PASSWORD=dev_admin
-
-PYTHONPATH=. alembic downgrade -1
--- db/migrations/scripts/migrate_db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export DB_URI=0.0.0.0
-export DB_PORT=5433
-export DB_NAME=boiler
-export DB_USER=boiler_user
-export DB_PASSWORD=dev_admin
-
-PYTHONPATH=. alembic revision --autogenerate
