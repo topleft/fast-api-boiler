e69a36cb9b36fc3aaa28691188884e9a2fd19242
Pete Jeffryes <pjeffryes@tendrilinc.com>
2020-11-03 10:53:56 -0600

    lots of refactoring

--- README.md
+++ README.md
@@ -1,15 +1,40 @@
-### Requirements:
+# Up Ping - Fast Api
 
-- json api
-    - FastAPI
-- postgres db
-    - sqlAlchemy
-    - Alembic
-- easy local dev
-    - docker compose
-- testing
-    - pytest
-- linting
-- terraform - GCP
+A project created by Orchestrated Energy to get started in GCP.
+
+# Quick Start Local Development
+
+**Start up the app:**
+```sh
+$ docker-compose up -d
+```
+
+**Stop the app:**
+```sh
+$ docker-compose down
+```
+
+**Lint and Test:**
+```sh
+$ sh ./docker_test.sh
+```
+
+**DB:**
 
 
+# CI and Deployment
+
+**On Commit (any branch except `master`)**:
+- Tests are run
+- docker image is built
+- `terraform plan` is run
+
+**On merge to `master`**:
+- docker image is built and pushed to registry
+- `terraform apply` is run and image is deployed to `dev`
+
+**On Tag**:
+- most recent docker image* is re-tagged
+- image is deployed to `stg`
+
+\* This is the most recent image built from what ever branch you are tagging
---
+++ project/db/migrations/env.py
@@ -0,0 +1,101 @@
+import os
+import sys
+from logging.config import fileConfig
+
+from alembic import context
+from sqlalchemy import create_engine, pool
+
+sys.path.insert(0, os.path.abspath('..'))
+from app.src.data.models.items import Base # noqa
+
+
+def get_url():
+    DB_URI = os.getenv("DB_URI")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+    return f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_URI}:{DB_PORT}/{DB_NAME}"
+
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+
+target_metadata = Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = get_url()
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        compare_type=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+
+    # this callback is used to prevent an auto-migration from being generated
+    # when there are no changes to the schema
+    # reference: http://alembic.zzzcomputing.com/en/latest/cookbook.html
+    def process_revision_directives(context, revision, directives):
+        if getattr(config.cmd_opts, 'autogenerate', False):
+            script = directives[0]
+            if script.upgrade_ops.is_empty():
+                directives[:] = []
+                print('No changes in schema detected.')
+
+    connectable = create_engine(get_url())
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+            process_revision_directives=process_revision_directives
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
